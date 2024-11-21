6c6f3145a618d102c4a8ce6d5d857abddc128c32
beginner <121371582+LSH160981@users.noreply.github.com>
2024-11-21 19:33:16 +0800

    Update install-Node.sh

--- install-Node.sh
+++ install-Node.sh
@@ -7,6 +7,33 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
+# 定义函数以获取当前 IPv4 地址
+ip_address() {
+    ipv4_address=$(curl -s --max-time 5 ipv4.ip.sb)
+    if [[ -n "$ipv4_address" ]]; then
+        echo -e "Current IPv4 address: ${ipv4_address}"
+    else
+        echo -e "Unable to fetch IPv4 address. Please check your network connection."
+    fi
+}
+
+# 安装必要的依赖
+function install_dependencies() {
+    echo "安装必要的依赖..."
+    apt update && apt upgrade -y
+    apt install -y curl wget gcc git
+}
+
+# 安装 Python 3.11 
+function install_python() {
+    echo "安装 Python 3.11..."
+    add-apt-repository ppa:deadsnakes/ppa -y
+    apt install -y python3.11 python3.11-venv python3.11-dev python3-pip
+
+    echo "验证 Python 版本..."
+    python3.11 --version
+}
+
 # 检查并安装 Node.js 和 npm
 function install_nodejs_and_npm() {
     # 检查Node.js是否安装
@@ -27,6 +54,9 @@ function install_nodejs_and_npm() {
     echo "npm版本：$(npm -v)"
 }
 
+ip_address
+install_dependencies
+install_python
 # 调用函数安装Node.js和npm
 install_nodejs_and_npm
 
